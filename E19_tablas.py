9f2d58721f40d0d2ff836459aadaadda52992486
Emanuelnore <emanuel.norena@udea.edu.co>
2025-06-01 09:12:56 -0500

    ahí ire mirando como es esto como se crea el proyecyo c omo se ve en git hub

---
+++ E19_tablas.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.common.by import By
+driver= webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.w3schools.com/html/html_tables.asp")
+time.sleep(5)
+
+valor=driver.find_element(By.XPATH, "//*[@id='customers']/tbody/tr").text
+
+print(valor)
+row=len(driver.find_elements(By.XPATH, "//*[@id='customers']/tbody/tr"))
+col =len(driver.find_elements(By.XPATH, "//*[@id='customers']/tbody/tr[1]/th"))
+print(row)
+
+print(col)
+for n in range(2,row+1):
+	for b in range(1,col+1):
+		xpath=f'//*[@id="customers"]/tbody/tr[{n}]/td[{b}]'
+		dato=driver.find_element(By.XPATH, xpath).text
+		print(dato,end=' ')
+	print()
